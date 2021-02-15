41118f1edd29a9c7df0c0610788318c8bac24167
KevinT935 <78836523+KevinT935@users.noreply.github.com>
2021-02-14 21:50:25 -0600

    update

--- src/Driver.java
+++ src/Driver.java
@@ -1,10 +1,16 @@
 
-public class Driver {
+public class Driver 
+{
 
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello World");
-
+		HeartstoneCard c1 = new HeartstoneCard("Armor Vendor", 1, 1, 3);
+		HeartstoneCard c2 = new HeartstoneCard("Wand Maker", 2, 2, 2);
+		c1.display();
+		c2.display();
+		
+		c1.name = "woot";
+		c1.display();
 	}
 
 }
---
+++ src/HeartstoneCard.java
@@ -0,0 +1,24 @@
+
+public class HeartstoneCard 
+{
+	int cost;
+	int attack;
+	int defense;
+	String name;
+	
+	public HeartstoneCard(String name, int cost, int attack, int defense)
+	{
+		this.cost = cost;
+		this.attack = attack;
+		this.defense = defense;
+		this.name = name;
+	}
+	
+	void display()
+	{
+		System.out.println("Name: " + this.name + "\nCost: " + this.cost + "\nAttack:" + this.attack + " Defense: " + this.defense);
+		System.out.format("Name: %s \n Cost: %d \n Attack: %d Defense: %d\n", this.name, this.cost, this.attack, this.defense);
+		
+	}
+}
+
